c1f06f316dfac38a858af905835692f99525f7d0
w1gz <private@mail.org>
2014-03-25 11:35:42 +0100

    restructure the project with traits

--- src/main/scala/lille1/car3/tpRest/RoutingService.scala
+++ src/main/scala/lille1/car3/tpRest/RoutingService.scala
@@ -1,9 +1,6 @@
 package lille1.car3.tpRest
 
-import java.io.File
-import java.io.File
-import java.io.FileInputStream
-import java.io.{ ByteArrayInputStream, InputStream, OutputStream, FileOutputStream, FileInputStream }
+import java.io.{ ByteArrayInputStream, InputStream, OutputStream, File, FileOutputStream, FileInputStream }
 
 import scala.util.matching.Regex
 
@@ -12,17 +9,15 @@ import org.apache.commons.net.ftp._
 import akka.actor.{ Props, Actor }
 
 import spray.http._
+import spray.http.BodyPart
 import spray.http.MediaTypes._
 import spray.httpx.unmarshalling.DeserializationError
-import spray.routing._
-import spray.http.BodyPart
 import spray.json._
-
-import DefaultJsonProtocol._
+import spray.routing._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
-class RoutingActor extends Actor with RoutingService {
+class RoutingActor extends Actor with RoutingService with HelperHtml {
 
   // the HttpService trait defines only one abstract member, which
   // connects the services environment to the enclosing actor or test
@@ -36,11 +31,11 @@ class RoutingActor extends Actor with RoutingService {
 }
 
 // this trait defines our service behavior independently from the service actor
-trait RoutingService extends HttpService {
+trait RoutingService extends HttpService with HelperHtml with HelperFunction {
   var current_connexion : Option[FtpConnexion] = None
 
   val default_route =
-    (path("") & get) { // `text/xml` by default, override to be sure
+    (path("") & get) {
       respondWithMediaType(`text/html`) {
         complete(loginForm)
       }
@@ -207,153 +202,4 @@ trait RoutingService extends HttpService {
   }
   // fin de la route !
 
-  def extract(text: String, regex: String, idx: Int) : String = {
-    val Some(reg) = regex.r.findFirstMatchIn(text)
-    reg.group(idx)
-  }
-
-  /* OVERRIDE OBJECTS */
-  object MyJsonProtocol extends DefaultJsonProtocol {
-    // on ajoute le type List[Map, Map] au types de bases supporté par Spray pouvant être sérialisé au format JSON
-    implicit object MyJsonFormat extends RootJsonFormat[List[Map[String, String]]] {
-      def returnJSONElement() : JsObject = JsObject("filename" -> JsString("Ta mere encule"), "chiffre" -> JsNumber(10))
-      def write(list: List[Map[String, String]]) = {
-        JsArray(list.map(elem => returnJSONElement))
-      }
-
-      def read(value: JsValue): List[Map[String, String]] = value match {
-        case JsArray(content) => {
-          var responseJSON : List[Map[String, String]] = List[Map[String, String]]()
-          var map : Map[String, String] = Map[String, String]()
-
-          for(JSON_elem <- content) {
-            JSON_elem.asJsObject.getFields("filename", "chiffre") match {
-              case Seq(JsString(filename), JsNumber(chiffre)) => map + ("filename" -> filename)
-              case _ => deserializationError("Erreur de formatage ~ le JSON attendu ne correspnd au format List[Map[String, String]]")
-            }
-
-            responseJSON = map :: responseJSON
-            /* ... */
-            map = Map[String, String]()
-          }
-
-          responseJSON
-        }
-        case _ => deserializationError("List[Map[String, String]] attendu")
-      }
-    }
-  }
-
-  import MyJsonProtocol._
-
-  /* HELPERS FUNCTIONS */
-  def JSON_ListResponse(files: Array[FTPFile]) : HttpResponse = {
-    var responseJSON : List[Map[String, String]] = List[Map[String, String]]()
-    var map : Map[String, String] = Map[String, String]()
-
-    for(f <- files) {
-      map + ("filename" -> f.getName())
-      responseJSON = map :: responseJSON
-      /* ... */
-      map = Map[String, String]()
-    }
-
-    HttpResponse(
-      status = 200,
-      entity = HttpEntity(`text/html`, responseJSON.toJson.toString)
-    )
-  }
-
-  def HTML_ListResponse(files: Array[FTPFile]) : HttpResponse = {
-    var responseHTML = new String("<html><head><title>Commande LIST - HTML</title></head><body><h1>Commande LIST FTP - Version HTML</h1><ul>")
-    for(f <- files) responseHTML += "<li>"+ f.getName() +"</li>"
-    responseHTML += "</ul></body></html>"
-
-    HttpResponse(
-      status = 200,
-      entity = HttpEntity(`text/html`, responseHTML)
-    )
-  }
-
-  lazy val loginForm = HttpResponse(
-    entity = HttpEntity(`text/html`,
-      <html>
-        <head>
-        <title>Passerelle REST</title>
-        </head>
-        <body>
-        <h1>Connexion - FTP</h1>
-
-      <form name="loginForm" method="post" action="loginAction">
-        <div>
-        <h3>Serveur FTP</h3>
-        <label for="server_ip">IP : </label><input type="text" name="server_ip"/>
-        <label for="server_port">Port : </label><input type="text" name="server_port"/>
-        </div>
-
-      <div>
-        <h3>Utilisateur</h3>
-        <label for="login">Identifiant : </label><input type="text" name="login_user"/><br/>
-        <label for="mdp">Mot de passe : </label><input type="password" name="mdp_user"/><br/>
-        <input type="submit" value="Se connecter" />
-        </div>
-        </form>
-        </body>
-        </html>.toString
-    )
-  )
-
-  lazy val storeForm = HttpResponse(
-    entity = HttpEntity(`text/html`,
-      <html>
-        <head>
-        <title>Déposer un fichier</title>
-        </head>
-        <body>
-        <h1>Commande STORE - Déposer votre fichier sur le serveur FTP</h1>
-        <form name="storeForm" method="post" enctype="multipart/form-data" action="store/file">
-        <input name="file" type="file" />
-        <input type="submit" value="Déposer" />
-        </form>
-        </body>
-        </html>.toString
-    )
-  )
-
-  lazy val listNote = HttpResponse(
-    entity = HttpEntity(`text/html`,
-      <html>
-        <head>
-        <title>Note LIST</title>
-        </head>
-        <body>
-        <h1>Utilisation de LIST</h1>
-        <p>
-        Pour effectuer correctement la commande LIST, veuillez utiliser :
-          <ul>
-        <li>L'URL <a href="list/html">list/html</a> pour visualiser LIST au format HTML</li>
-        <li>L'URL <a href="list/json">list/json</a> pour visualiser LIST au format JSON</li>
-        </ul>
-        </p>
-        </body>
-        </html>.toString
-    )
-  )
-
-  lazy val error404Html = HttpResponse(
-    status = 404,
-    entity = HttpEntity(`text/html`,
-      <html>
-        <head>
-        <title>Erreur 404</title>
-        </head>
-        <body>
-        <h1>Erreur 404</h1>
-        <p>
-        La page demandée est inconnue
-        </p>
-        </body>
-        </html>.toString
-    )
-  )
 }
